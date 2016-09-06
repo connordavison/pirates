4ac6d5b0a8761bf7df5c6847abff10a6cc85019b
Connor Davison <connorpdavison@gmail.com>
2016-09-06 16:47:58 +0100

    Added tests for isSunk and decreaseHealth.

--- test/ShipTest.php
+++ test/ShipTest.php
@@ -9,4 +9,23 @@ class ShipTest extends \PHPUnit_Framework_TestCase
         $ship = new Ship(15, 0);
         $this->assertEquals(100, $ship->getHealth());
     }
+
+    public function testHealthCanDecrease()
+    {
+        $ship = new Ship(15, 0);
+        $ship->decreaseHealth(10);
+        $this->assertEquals(90, $ship->getHealth());
+    }
+
+    public function testShipSinks()
+    {
+        $ship = new Ship(15, 0);
+        $this->assertFalse($ship->isSunk());
+
+        $ship->decreaseHealth(100);
+        $this->assertTrue($ship->isSunk());
+
+        $ship->decreaseHealth(100);
+        $this->assertTrue($ship->isSunk());
+    }
 }
